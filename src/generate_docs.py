a9c51281f190335c77cdaef96f71beda129c4464
Michael Sass Hansen <michael@criterion.ai>
2020-11-22 22:10:30 +0100

    Cleaned up documents

--- src/generate_docs.py
+++ src/generate_docs.py
@@ -55,7 +55,7 @@ def batch_convert(code_dir="src", generated_dir = "generated",
 
                 colab_link = f"[{os.path.splitext(os.path.basename(notebook))[0].replace('_', ' ').title()}](https://githubtocolab.com/criterion-ai/brevettiai-docs/blob/master/{'/'.join(notebook.split(os.path.sep))})"
                 tutorial_links.append(colab_link)
-                new_intro = output_buffer[:np.where(np.array(list(output_buffer))=="#")[0][1]]
+                new_intro = output_buffer[:np.where(np.array(list(output_buffer))=="\n# ")[0][1]]
                 outro = f"""To explore the code by examples, please run the in the notebook that can be found on colab on this link {colab_link}"""
                 open(output_file, "w").write(new_intro + outro)
     open("SUMMARY.md", "w").write("\n".join(summary_doc.lines.values.tolist()))
